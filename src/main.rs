9bbc4ae0f68cb007bfb79ab14a97a4f85de7d738
Gabriel <gabrielpfernandez@gmail.com>
2024-08-04 23:47:52 +0200

    Introducing websockets instead of http. Creating a trade builder. Subscribing to a trading platform

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use crate::trader::trader::Trader;
+use crate::trader::trader::{Trader, TraderBuilder};
 
 mod trader;
 
 #[tokio::main]
 async fn main() {
-    let trader = match Trader::new("./endpoints.dat") {
+    let trader = match TraderBuilder::new("./endpoints.dat") {
         Ok(t) => t,
         Err(e) => panic!("Cannot create trader: {}", e),
     };
--- src/trader.rs
+++ src/trader.rs
@@ -1,32 +1,88 @@
 pub mod trader {
 
-    use reqwest::Client;
+    use websocket::{ClientBuilder, Message};
+    use std::net::TcpStream;
+    use std::collections::HashMap;
     use std::fs;
     use std::str::from_utf8;
+    use thiserror::Error;
+    use websocket::client::ParseError;
+    use websocket::result::WebSocketResult;
+    use websocket::sync::Client;
+    use websocket::WebSocketError;
 
-    pub struct Trader {
-        endpoints: Vec<String>,
-        client: Client,
+    #[derive(Error, Debug, derive_more::From, derive_more::Display)]
+    pub enum ClientError {
+        UrlParseError(ParseError),
+        SocketConnectError(WebSocketError),
     }
 
-    impl Trader {
-        pub fn new(endpoint_file_path: &str) -> Result<Self, Box<dyn std::error::Error + 'static>> {
-            let data = fs::read(&endpoint_file_path)?;
-            let endpoint_str = match from_utf8(&data) { 
+    pub struct TraderBuilder {
+        endpoints: HashMap<String, Vec<String>>,
+    }
+
+    pub struct Trader {
+        endpoint: String,
+        subs_endpoint: String,
+        client: Client<TcpStream>,
+    }
+
+    impl TraderBuilder {
+        pub fn new(endpoint_file_path: &str, 
+            subs_endpoint_file_path: &str) -> Result<Self, Box<dyn std::error::Error + 'static>>  { 
+            let ep_data = fs::read(&endpoint_file_path)?;
+            let endpoint_str = match from_utf8(&ep_data) { 
                 Ok(s) => s,
                 Err(e) => panic!("File {} is corrupted. Failed with error: {}", endpoint_file_path, e),
             };
             let endpoints_slc: Vec<&str> = endpoint_str.split("\n").collect();
-            let endpoints = endpoints_slc.iter().map(|slc| {
+            let endpoint_lines: Vec<String> = endpoints_slc.iter().map(|slc| {
                     slc.to_string()
                 }).collect();
-            
-            let client = Client::new();
-            Ok(Trader {
+            let endpoints: HashMap<String, Vec<String>> = endpoint_lines
+                .iter()
+                .map(|line| {
+                    let vals: Vec<&str> = line.split(",").collect();
+                    (vals[0].to_string(), vec![vals[1].to_string(), vals[2].to_string()])
+                }).collect();
+
+            Ok(TraderBuilder {
                 endpoints,
+            })
+        }
+
+
+        pub fn build(&self, key: String) -> Option<Trader> {
+            match self.endpoints.get(key) {
+                Some(ep) => Some(Trader::new(ep[0], ep[1])),
+                None => None,
+           }
+        }
+    }
+
+    impl Trader {
+        pub fn new(endpoint: String, subs_endpoint: String) -> Result<Self, ClientError> {
+            let client = ClientBuilder::new(endpoint)?
+                .connect()?;
+            Ok(Trader {
+                endpoint,
                 client,
             })
         }
+
+        pub fn subscribe(&self) -> Result<(), ClientError>{
+            let msg = self.subscribe_msg(1);
+            self.client.send_message(&Message::text(&msg))?
+        }
+
+        
+        fn subscribe_msg(&self, id: u32) -> String { 
+            let raw = format!(r#"{{"method": "SUBSCRIBE",
+                          "params": ["{}"], "id": {}}}"#, self.subs_endpoint, id);
+            println!("raw: {}", &raw);
+            raw
+        }
+
     }
 }
 
